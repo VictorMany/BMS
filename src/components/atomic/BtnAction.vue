45f53c3dbf46a19f3a5eeecac386e16089a90686
Victor Manuel Velazquez Fuentes <vvelazquez@bapcorporativo.com>
2022-10-05 22:56:16 -0500

    add BtnAction component

---
+++ src/components/atomic/BtnAction.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-btn class="btn-style q-pr-sm"
+    :unelevated="btnUnelevated"
+    :rounded="btnRounded"
+    :no-caps="btnCaps"
+    :text-color="btnColor"
+    :label="btnTitle"
+    @click="btnAction">
+    <div v-if="iconName" class="row items-center no-wrap">
+      <q-icon right :name="iconName" />
+    </div>
+  </q-btn>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BtnAction',
+  props: {
+    btnTitle: {
+      type: String,
+      default: 'Agregar',
+      required: true
+    },
+
+    btnColor: {
+      type: String,
+      required: false,
+      default: 'rgba(76, 165, 243, 0.8)'
+    },
+
+    btnBackground: {
+      type: String,
+      required: false,
+      default: '#C6DFF1'
+    },
+
+    btnRounded: {
+      type: Boolean,
+      default: true,
+      required: false
+    },
+
+    btnUnelevated: {
+      type: Boolean,
+      default: true,
+      required: false
+    },
+
+    btnAction: {
+      type: Function,
+      default: () => {},
+      required: false
+    },
+
+    btnCaps: {
+      type: Boolean,
+      default: true,
+      required: false
+    },
+
+    iconName: {
+      type: String,
+      required: false,
+      default: 'add_circle'
+    }
+  }
+})
+</script>
+
+<style>
+.btn-style {
+  background-color: v-bind(btnBackground);
+  color: v-bind(btnColor);
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 18px;
+}
+</style>
