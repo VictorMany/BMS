0e5608930abe1341c9c8f4895f7af046d9fb2e98
Victor Manuel Velazquez Fuentes <vicmany@MacBook-Air-de-Victor.local>
2023-02-07 00:58:08 -0600

    added fecha de creacion to equipment detail

--- src/pages/Equipments/DetailEquipment.vue
+++ src/pages/Equipments/DetailEquipment.vue
@@ -9,9 +9,14 @@
         :btn-action="btnAction"
       />
       <div class="main-container-page" style="height: 82%">
-        <q-scroll-area class="fit" :thumb-style="{ right: '6px', borderRadius: '5px', background: 'rgba(135, 192, 232, 0.44)', width: '5px', opacity: 1 }">
+        <q-scroll-area class="full-height" style="height: 95% !important" :thumb-style="{ right: '6px', borderRadius: '5px', background: 'rgba(135, 192, 232, 0.44)', width: '5px', opacity: 1 }">
           <form-label :textfields="textfields" />
         </q-scroll-area>
+        <div class="col-12" style="background-color: #e7f0f7; height: 5%;">
+          <div class="form__date column items-end q-pa-sm q-mt-auto">
+            <div>Fecha de creación <strong> 12/02/2022</strong></div>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
