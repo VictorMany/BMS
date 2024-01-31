9b66b547eac6c7fc1493258aa1d237b3604cc5fe
Victor Velazquez <vvelazquez@bapcorporativo.com>
2024-01-31 01:00:43 -0600

    adding a menu left

--- src/components/compose/HeaderActions.vue
+++ src/components/compose/HeaderActions.vue
@@ -1,8 +1,64 @@
 <!-- eslint-disable vue/no-mutating-props -->
 <template>
   <div class="container-header row items-center q-ma-none q-pa-none q-mb-sm">
+    <div class="col-xs-auto column content-end">
+      <!-- <btn-action v-bind="btnMenu" /> -->
+
+      <q-fab
+        ref="menu"
+        v-model="fab2"
+        icon="widgets"
+        vertical-actions-align="left"
+        color="secondary"
+        push
+        square
+        :persistent="false"
+        label="Menú"
+        label-position="right"
+        external-label
+        padding="xs"
+        direction="down"
+      >
+        <q-fab-action
+          color="blue-10"
+          icon="mail"
+          label="Equipos"
+          to="equipments"
+        />
+        <q-fab-action
+          color="blue-9"
+          to="maintenances"
+          icon="alarm"
+          label="Mantenimientos"
+        />
+        <q-fab-action
+          color="blue-8"
+          to="reports"
+          icon="airplay"
+          label="Reportes"
+        />
+        <q-fab-action
+          color="blue-7"
+          to="calendar"
+          icon="room"
+          label="Calendario"
+        />
+        <q-fab-action
+          color="blue-6"
+          to="maintenances-plan"
+          icon="room"
+          label="Planes"
+        />
+        <q-fab-action
+          color="blue-5"
+          to="settings"
+          icon="room"
+          label="Configuración"
+        />
+      </q-fab>
+    </div>
     <div class="col-xs-auto col-sm title-page">
-      {{ titlePage }}
+      {{ titlePage.toLocaleUpperCase() }}
     </div>
     <div
       v-if="btnAction.show"
@@ -85,8 +141,20 @@ export default defineComponent({
     return {
       searchModelLocal: this.searchModel,
       switchContentLocal: this.switchContent,
+      fab2: false,
     };
   },
+
+  // async beforeRouteUpdate(to, from, next) {
+  //   try {
+  //     this.fab2 = false
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   next();
+  // },
+
+
   watch: {
     searchModelLocal(value) {
       this.$emit('update:searchModel', value);
