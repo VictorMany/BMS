23ad6c5c20a990a74cefa58bff93576c44e7a783
Victor Velazquez <vvelazquez@bapcorporativo.com>
2024-03-30 21:36:01 -0600

    fixing formatted date with just date and not time

--- src/store/contracts/getters.js
+++ src/store/contracts/getters.js
@@ -1,11 +1,11 @@
-import { formatDate } from '../global/actions';
+import { formatDateOnly } from '../global/actions';
 
 export function getContractsGetter(state) {
     let serviceContract = state.contracts.map(contract => ({
         id: contract.ContractId,
         contractName: contract.contractName,
         user: contract.User?.userName,
-        dates: contract.comodato ? 'Contrato comodato' : formatDate(contract.startDate) + ' - ' + formatDate(contract.endDate),
+        dates: contract.comodato ? 'Contrato comodato' : formatDateOnly(contract.startDate) + ' - ' + formatDateOnly(contract.endDate),
         comodato: contract.comodato
     }));
     return serviceContract
